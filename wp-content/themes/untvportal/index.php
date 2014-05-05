a1a26b11446618d4ebae5e3ff22e2629420be72d
Jeth <jethdeguzman@gmail.com>
2014-05-05 12:03:43 +0800

    update

--- wp-content/themes/untvportal/index.php
+++ wp-content/themes/untvportal/index.php
@@ -11,7 +11,7 @@
 					  						  								<div class="large-8 medium-8 small-12 columns slideItem">
 					           
 					            <a href="http://ava-themes.com/WP/United/converting-to-led-lights/" >
-					              <img width="683" height="426" src="http://www.untvweb.com/wp-content/files_mf/cache/th_2182122c6b5fe6c9cc0f4a92521738fa_1399022516CFEGDA.png" alt="10347464594_cbb806f238_b" style="height:523px;" />	              
+					              <img width="683" height="426" src="http://www.untvweb.com/wp-content/files_mf/cache/th_2182122c6b5fe6c9cc0f4a92521738fa_1399022516CFEGDA.png" alt="10347464594_cbb806f238_b" style="height:498px;" />	              
 					              <div class="textHold">
 					                <div class="large-12 columns">
 					                  <div class="date">07<span>Apr</span><span class="line"></span></div>
@@ -32,7 +32,7 @@
 					  								<div class="large-4 medium-4 small-12 columns slideItem">
 					          
 					            <a href="http://ava-themes.com/WP/United/your-imagination-your-freedom/">
-					              <img width="441" height="275" src="http://www.untvweb.com/wp-content/files_mf/cache/th_2182122c6b5fe6c9cc0f4a92521738fa_1398829831ACTIONCENTERGRAPHICSwebBANNER_APR302014.png" class="attachment-slider_small wp-post-image" alt="6940343109_8e0633088e_b" style="height:275px;">					              <div class="textHold">
+					              <img width="441" height="275" src="http://www.untvweb.com/wp-content/files_mf/cache/th_2182122c6b5fe6c9cc0f4a92521738fa_1398829831ACTIONCENTERGRAPHICSwebBANNER_APR302014.png" class="attachment-slider_small wp-post-image" alt="6940343109_8e0633088e_b" style="height:249px;">					              <div class="textHold">
 					                <div class="large-12 columns">
 					                  <div class="date">03<span>Apr</span><span class="line"></span></div>
 					                </div>
@@ -48,28 +48,19 @@
 					              </div>              
 					            </a>	            			
 							</div>
-													<div class="large-4 medium-4 small-12 columns slideItem">
-					            <div class="newSocials">
-					              	<ul>
-					              		<li>
-					              			<a href="https://plus.google.com/share?url=http://ava-themes.com/WP/United/you-dont-have-to-go-with-the-crowd/"  target="blank"><i class="fa fa-google-plus"></i></a>
-					              		</li>
-					              		<li>
-					              			<a class="" href="http://twitter.com/share?url=http://ava-themes.com/WP/United/you-dont-have-to-go-with-the-crowd/&text=You don&#8217;t have to go with the crowd &#8211; Sudanese supermodel Alek Wek on United" target="_blank"><i class="fa fa-twitter"></i></a>
-					              		</li>
-					              		<li>
-					              			<a class="" href="http://www.facebook.com/sharer.php?u=http://ava-themes.com/WP/United/you-dont-have-to-go-with-the-crowd/&t=You don&#8217;t have to go with the crowd &#8211; Sudanese supermodel Alek Wek" target="blank"><i class="fa fa-facebook"></i></a>
-					              		</li>
-					              	</ul>
-					            </div>	
+							<div class="large-4 medium-4 small-12 columns slideItem">
+					            
 					            <a href="http://ava-themes.com/WP/United/you-dont-have-to-go-with-the-crowd/">
-					              <img width="441" height="275" src="http://ava-themes.com/WP/United/wp-content/uploads/2014/04/9720369538_b6e96cb5ee_b-441x275.jpg" class="attachment-slider_small wp-post-image" alt="9720369538_b6e96cb5ee_b" />					              <div class="textHold">
+					              <img width="441" height="275" src="http://www.untvweb.com/wp-content/files_mf/cache/th_2182122c6b5fe6c9cc0f4a92521738fa_1397096148OFWBannerHomepage.png" class="attachment-slider_small wp-post-image" alt="9720369538_b6e96cb5ee_b"  style="height:249px; " />					              <div class="textHold">
 					                <div class="large-12 columns">
 					                  <div class="date">02<span>Apr</span><span class="line"></span></div>
 					                </div>
 					                <div class="large-12 columns textHoldBottom">
 					                  <div class="large-8 medium-8 columns">
-					                    <h1>You don&#8217;t have to go with the crowd &#8211; Sudanese supermodel Alek Wek</h1>
+					                    <h1>My OFW Stories</h1>
+					                    <p>UNTV launches new program featuring the inspiring stories and journey of the country's new economic heroes. 
+
+					                    </p>
 					                  </div>
 					                  <div class="large-12 medium-12 columns moreBottom">
 					                    <span  class="more">Read More</span>
@@ -83,27 +74,15 @@
 
 				    <li>
 					  						  							<div class="large-4 medium-4 small-12 columns slideItem">
-				            <div class="newSocials">
-				              	<ul>
-				              		<li>
-				              			<a href="https://plus.google.com/share?url=http://ava-themes.com/WP/United/paris-fashion-week-how-it-rewrote-the-rules/"  target="blank"><i class="fa fa-google-plus"></i></a>
-				              		</li>
-				              		<li>
-				              			<a class="" href="http://twitter.com/share?url=http://ava-themes.com/WP/United/paris-fashion-week-how-it-rewrote-the-rules/&text=Paris fashion week: how it rewrote the rules on United" target="_blank"><i class="fa fa-twitter"></i></a>
-				              		</li>
-				              		<li>
-				              			<a class="" href="http://www.facebook.com/sharer.php?u=http://ava-themes.com/WP/United/paris-fashion-week-how-it-rewrote-the-rules/&t=Paris fashion week: how it rewrote the rules" target="blank"><i class="fa fa-facebook"></i></a>
-				              		</li>
-				              	</ul>
-				            </div>	
+				           
 				            <a href="http://ava-themes.com/WP/United/paris-fashion-week-how-it-rewrote-the-rules/" >
-				              <img width="441" height="275" src="http://ava-themes.com/WP/United/wp-content/uploads/2014/04/fw5-441x275.jpg" class="attachment-slider_small wp-post-image" alt="fw5" />				              <div class="textHold">
+				              <img width="441" height="275" src="http://www.untvweb.com/wp-content/files_mf/cache/th_2182122c6b5fe6c9cc0f4a92521738fa_1392621994UNTVAWARDSBANNER.png" class="attachment-slider_small wp-post-image" alt="fw5" style="height:249px;" />				              <div class="textHold">
 				                <div class="large-12 columns">
 				                  <div class="date">02<span>Apr</span><span class="line"></span></div>
 				                </div>
 				                <div class="large-12 columns textHoldBottom">
 				                  <div class="large-8 medium-8 columns">
-				                    <h1>Paris fashion week: how it rewrote the rules</h1>
+				                    <h1>PUP Honors Daniel Razon and UNTV</h1>
 				                  </div>
 				                  <div class="large-12 medium-12 columns moreBottom">
 				                    <span  class="more">Read More</span>
@@ -113,27 +92,15 @@
 				            </a>
 				        </div>
 												<div class="large-4 medium-4 small-12 columns slideItem">
-				            <div class="newSocials">
-				              	<ul>
-				              		<li>
-				              			<a href="https://plus.google.com/share?url=http://ava-themes.com/WP/United/how-to-master-deviation-chic/"  target="blank"><i class="fa fa-google-plus"></i></a>
-				              		</li>
-				              		<li>
-				              			<a class="" href="http://twitter.com/share?url=http://ava-themes.com/WP/United/how-to-master-deviation-chic/&text=How to master &#8216;deviation chic&#8217; &#8211; Dressing down is the new power dressing on United" target="_blank"><i class="fa fa-twitter"></i></a>
-				              		</li>
-				              		<li>
-				              			<a class="" href="http://www.facebook.com/sharer.php?u=http://ava-themes.com/WP/United/how-to-master-deviation-chic/&t=How to master &#8216;deviation chic&#8217; &#8211; Dressing down is the new power dressing" target="blank"><i class="fa fa-facebook"></i></a>
-				              		</li>
-				              	</ul>
-				            </div>	
+				            
 				            <a href="http://ava-themes.com/WP/United/how-to-master-deviation-chic/" >
-				              <img width="441" height="275" src="http://ava-themes.com/WP/United/wp-content/uploads/2014/04/fashion-441x275.jpg" class="attachment-slider_small wp-post-image" alt="fashion" />				              <div class="textHold">
+				              <img width="441" height="275" src="http://www.untvweb.com/wp-content/files_mf/cache/th_2182122c6b5fe6c9cc0f4a92521738fa_1387419193BannerAd.jpg" class="attachment-slider_small wp-post-image" alt="fashion" style="height:249px;" />				              <div class="textHold">
 				                <div class="large-12 columns">
 				                  <div class="date">02<span>Apr</span><span class="line"></span></div>
 				                </div>
 				                <div class="large-12 columns textHoldBottom">
 				                  <div class="large-8 medium-8 columns">
-				                    <h1>How to master &#8216;deviation chic&#8217; &#8211; Dressing down is the new power dressing</h1>
+				                    <h1>Anak TV Awards</h1>
 				                  </div>
 				                  <div class="large-12 medium-12 columns moreBottom">
 				                    <span  class="more">Read More</span>
@@ -143,27 +110,16 @@
 				            </a>
 				        </div>
 												<div class="large-4 medium-4 small-12 columns slideItem">
-				            <div class="newSocials">
-				              	<ul>
-				              		<li>
-				              			<a href="https://plus.google.com/share?url=http://ava-themes.com/WP/United/crimes-of-the-hot-9/"  target="blank"><i class="fa fa-google-plus"></i></a>
-				              		</li>
-				              		<li>
-				              			<a class="" href="http://twitter.com/share?url=http://ava-themes.com/WP/United/crimes-of-the-hot-9/&text=How to eat: a ploughman&#8217;s lunch &#8211; Food tips on United" target="_blank"><i class="fa fa-twitter"></i></a>
-				              		</li>
-				              		<li>
-				              			<a class="" href="http://www.facebook.com/sharer.php?u=http://ava-themes.com/WP/United/crimes-of-the-hot-9/&t=How to eat: a ploughman&#8217;s lunch &#8211; Food tips" target="blank"><i class="fa fa-facebook"></i></a>
-				              		</li>
-				              	</ul>
-				            </div>	
+				           
 				            <a href="http://ava-themes.com/WP/United/crimes-of-the-hot-9/" >
-				              <img width="441" height="275" src="http://ava-themes.com/WP/United/wp-content/uploads/2014/03/food-441x275.jpg" class="attachment-slider_small wp-post-image" alt="food" />				              <div class="textHold">
+				              <img width="441" height="275" src="http://www.untvweb.com/wp-content/files_mf/cache/th_2182122c6b5fe6c9cc0f4a92521738fa_138254748633aniv.jpg" class="attachment-slider_small wp-post-image" alt="food" style="height:249px;"/>				              <div class="textHold">
 				                <div class="large-12 columns">
 				                  <div class="date">21<span>Mar</span><span class="line"></span></div>
 				                </div>
 				                <div class="large-12 columns textHoldBottom">
 				                  <div class="large-8 medium-8 columns">
-				                    <h1>How to eat: a ploughman&#8217;s lunch &#8211; Food tips</h1>
+				                    <12>The Old Path</h1>
+				                    <p>33 Years - Building a Prayerful Nation through Broadcast Evangelization.</p>
 				                  </div>
 				                  <div class="large-12 medium-12 columns moreBottom">
 				                    <span  class="more">Read More</span>
@@ -173,27 +129,16 @@
 				            </a>
 				        </div>
 												<div class="large-4 medium-4 small-12 columns slideItem">
-				            <div class="newSocials">
-				              	<ul>
-				              		<li>
-				              			<a href="https://plus.google.com/share?url=http://ava-themes.com/WP/United/crimes-of-the-hot-4/"  target="blank"><i class="fa fa-google-plus"></i></a>
-				              		</li>
-				              		<li>
-				              			<a class="" href="http://twitter.com/share?url=http://ava-themes.com/WP/United/crimes-of-the-hot-4/&text=Daylight saving switch and lost sleep increase heart attack risk on United" target="_blank"><i class="fa fa-twitter"></i></a>
-				              		</li>
-				              		<li>
-				              			<a class="" href="http://www.facebook.com/sharer.php?u=http://ava-themes.com/WP/United/crimes-of-the-hot-4/&t=Daylight saving switch and lost sleep increase heart attack risk" target="blank"><i class="fa fa-facebook"></i></a>
-				              		</li>
-				              	</ul>
-				            </div>	
+				          
 				            <a href="http://ava-themes.com/WP/United/crimes-of-the-hot-4/" >
-				              <img width="441" height="275" src="http://ava-themes.com/WP/United/wp-content/uploads/2014/03/sunrise3-441x275.jpg" class="attachment-slider_small wp-post-image" alt="ed hardy &amp; splash one I" />				              <div class="textHold">
+				              <img width="441" height="275" src="http://www.untvweb.com/wp-content/files_mf/cache/th_2182122c6b5fe6c9cc0f4a92521738fa_1369223614bmpi_studio.jpg" class="attachment-slider_small wp-post-image" alt="ed hardy &amp; splash one I" style="height:249px;" />				              <div class="textHold">
 				                <div class="large-12 columns">
 				                  <div class="date">21<span>Mar</span><span class="line"></span></div>
 				                </div>
 				                <div class="large-12 columns textHoldBottom">
 				                  <div class="large-8 medium-8 columns">
-				                    <h1>Daylight saving switch and lost sleep increase heart attack risk</h1>
+				                   <h1>BMPI RECORDING STUDIO</h1>
+				                   <p>For inquiries regarding studio bookings and procedures, you may text or call: &nbsp;0926-5559339 &nbsp;More Info</p>
 				                  </div>
 				                  <div class="large-12 medium-12 columns moreBottom">
 				                    <span  class="more">Read More</span>
@@ -203,27 +148,15 @@
 				            </a>
 				        </div>
 												<div class="large-4 medium-4 small-12 columns slideItem">
-				            <div class="newSocials">
-				              	<ul>
-				              		<li>
-				              			<a href="https://plus.google.com/share?url=http://ava-themes.com/WP/United/crimes-of-the-hot-2/"  target="blank"><i class="fa fa-google-plus"></i></a>
-				              		</li>
-				              		<li>
-				              			<a class="" href="http://twitter.com/share?url=http://ava-themes.com/WP/United/crimes-of-the-hot-2/&text=The park-bench workout: five simple exercises on United" target="_blank"><i class="fa fa-twitter"></i></a>
-				              		</li>
-				              		<li>
-				              			<a class="" href="http://www.facebook.com/sharer.php?u=http://ava-themes.com/WP/United/crimes-of-the-hot-2/&t=The park-bench workout: five simple exercises" target="blank"><i class="fa fa-facebook"></i></a>
-				              		</li>
-				              	</ul>
-				            </div>	
+				            
 				            <a href="http://ava-themes.com/WP/United/crimes-of-the-hot-2/" >
-				              <img width="441" height="275" src="http://ava-themes.com/WP/United/wp-content/uploads/2014/03/bench-441x275.jpg" class="attachment-slider_small wp-post-image" alt="bench" />				              <div class="textHold">
+				              <img width="441" height="275" src="http://www.untvweb.com/wp-content/files_mf/cache/th_2182122c6b5fe6c9cc0f4a92521738fa_1346336939GIS.jpg" class="attachment-slider_small wp-post-image" alt="bench" style="height:249px;" />				              <div class="textHold">
 				                <div class="large-12 columns">
 				                  <div class="date">21<span>Mar</span><span class="line"></span></div>
 				                </div>
 				                <div class="large-12 columns textHoldBottom">
 				                  <div class="large-8 medium-8 columns">
-				                    <h1>The park-bench workout: five simple exercises</h1>
+				                    <h1>Get It Straight with Daniel Razon</h1>
 				                  </div>
 				                  <div class="large-12 medium-12 columns moreBottom">
 				                    <span  class="more">Read More</span>
